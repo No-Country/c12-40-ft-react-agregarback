91bb93c033c75eb92ba5572a96456ecf949adede
GonzaloArray <gonzalo.arrayaran@gmail.com>
2023-06-30 14:11:08 -0300

    feat: firebase

---
+++ src/service/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from 'firebase/app'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyBnk8tGAX16Iaz-Cl47VpJi2wjYHjlZpGg',
+  authDomain: 'c12-40-ft.firebaseapp.com',
+  projectId: 'c12-40-ft',
+  storageBucket: 'c12-40-ft.appspot.com',
+  messagingSenderId: '467734432296',
+  appId: '1:467734432296:web:68c1e2484146366c8b831d',
+  measurementId: 'G-4X4J83N3T6'
+}
+
+const app = initializeApp(firebaseConfig)
