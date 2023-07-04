93f99fe557016648f7e7c02d2496a90d2348edae
Francisco Carvajal Villegas <57154324+FranciscoCarvajal404@users.noreply.github.com>
2023-07-04 11:30:20 -0600

    feat: dashboard layout

--- src/client/page/dashboard/layout/LayoutDashboard.jsx
+++ src/client/page/dashboard/layout/LayoutDashboard.jsx
@@ -1,18 +1,63 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import { styled } from 'styled-components'
+
+const Layout = styled.div`
+
+  display: flex;
+  width: 100%;
+`
+
+const Aside = styled.aside`
+
+  background-color: green;
+
+  min-height: 100vh;
+  max-width: 30%;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  padding: 32px;
+
+`
+
+const InnerLayout = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  width: 100%;
+`
+
+const Header = styled.header`
+
+  background-color: red;
+
+  min-width: 60%;
+`
+
+const Main = styled.main`
+  background-color: blue;
+
+  height: 100%;
+`
 
 export const LayoutDashboard = () => {
   return (
-    <div>
-      <header>
-        header
-      </header>
-      <aside>
-        aside
-      </aside>
-      <main>
-        <Outlet />
-      </main>
-    </div>
+    <Layout>
+      <Aside>
+        Aside
+      </Aside>
+      <InnerLayout>
+        <Header>
+          Header
+        </Header>
+        <Main>
+          <Outlet />
+        </Main>
+      </InnerLayout>
+    </Layout>
   )
 }
