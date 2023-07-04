61fd71c5c793838092eab14cdf43ef26ae387f59
GonzaloArray <gonzalo.arrayaran@gmail.com>
2023-07-04 13:29:02 -0300

    feat: new router

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { Routes } from "react-router-dom"
 import { Page as Login } from "./auth/page/login/Page"
 import { Page as Register } from "./auth/page/register/Page"
 import { Page as Chat } from "./client/page/chat/Page"
+import { Page as Home } from "./client/page/home/Page"
+import { Page as Dashboard } from "./client/page/dashboard/Page"
 import { useSelector } from 'react-redux'
 
 function App () {
@@ -14,7 +16,8 @@ function App () {
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/chat" element={<Chat />} />
-
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/" element={<Home />} />
       </Routes>
     </>
   )
