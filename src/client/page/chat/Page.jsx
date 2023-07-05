6fb64ae0fde13f7ab3d510a732367a35b8b841df
TorlettiJoaquin <Axeltorletti@gmail.com>
2023-07-05 15:08:29 -0300

    feat: chat components

--- src/client/page/chat/Page.jsx
+++ src/client/page/chat/Page.jsx
@@ -1,9 +1,25 @@
 // Esta es la pagina de chat, aqui se mostrara el chat y se podra enviar mensajes
 
 import React from 'react'
+import Sidebar from './components/Sidebar'
+import { styled } from 'styled-components'
 
 export const Page = () => {
   return (
-    <div>Chat</div>
+    <div>
+      <h1>Chat</h1>
+      <Sidebar />
+    </div>
   )
 }
+
+// styles components abajo
+
+styled.div`
+  background-color: #fff;
+  border-radius: 10px;
+  box-shadow: 0 0 10px rgba(0,0,0,0.1);
+  height: 100%;
+  overflow: hidden;
+  width: 100%;
+`
