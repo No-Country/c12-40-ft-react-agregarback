d066676931e7a6243b123d109cce7901b9732e00
GonzaloArray <gonzalo.arrayaran@gmail.com>
2023-07-05 17:40:07 -0300

    feat: input register and login

--- src/auth/components/Input.jsx
+++ src/auth/components/Input.jsx
@@ -1,5 +1,8 @@
-export const Input = () => {
+export const Input = ({type, name, change }) => {
   return (
-    <div>Input</div>
+    <div>
+      <label htmlFor={name}></label>
+      <input type={type} name={name} id={name} onChange={change} />
+    </div>
   )
 }
