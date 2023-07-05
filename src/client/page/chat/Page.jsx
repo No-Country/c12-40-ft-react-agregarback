4c5abba3bf6733ecba502bbc538a78e2298fb338
TorlettiJoaquin <Axeltorletti@gmail.com>
2023-07-05 03:57:38 -0300

    add: package react-firebase-hooks

--- src/client/page/chat/Page.jsx
+++ src/client/page/chat/Page.jsx
@@ -1,16 +1,54 @@
 // Esta es la pagina de chat, aqui se mostrara el chat y se podra enviar mensajes
 
-// import { useEffect, useState } from "react";
-// import firebase from "firebase/app";
+import { useEffect, useState } from "react";
+import firebase from "firebase/app";
 import "firebase/firestore";
 
 export const Page = () => {
+    const [messages, setMessages] = useState([]);
+    const [newMessage, setNewMessage] = useState("");
+
+    useEffect(() => {
+        const fetchMessages = async () => {
+            const snapshot = await firebase
+                .firestore()
+                .collection("messages")
+                .get();
+            const messagesData = snapshot.docs.map((doc) => doc.data());
+            setMessages(messagesData);
+        };
+        fetchMessages();
+    }, []);
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        if (newMessage.trim() === "") return;
+
+        await firebase.firestore().collection("messages").add({
+            text: newMessage.trim(),
+            timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+        });
+
+        setNewMessage("");
+    };
+
     return (
         <div>
             <h1>Chat</h1>
             <ul>
-                <li>HOLA MUNDO</li>
+                {messages.map((message, index) => (
+                    <li key={index}>{message.text}</li>
+                ))}
             </ul>
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    value={newMessage}
+                    onChange={(e) => setNewMessage(e.target.value)}
+                    placeholder="Escribe un mensaje..."
+                />
+                <button type="submit">Enviar</button>
+            </form>
         </div>
     );
 };
