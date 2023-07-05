73f554e6ebece91c954e8c1594d3751c9da1b693
TorlettiJoaquin <Axeltorletti@gmail.com>
2023-07-05 03:05:54 -0300

    fix: chat page

--- src/client/page/chat/Page.jsx
+++ src/client/page/chat/Page.jsx
@@ -1,54 +1,65 @@
-//Esta es la pagina de chat, aqui se mostrara el chat y se podra enviar mensajes
+// Esta es la pagina de chat, aqui se mostrara el chat y se podra enviar mensajes
 
-import React, { useEffect, useState } from "react";
-import firebase from "firebase/app";
+// import { useEffect, useState } from "react";
+// import firebase from "firebase/app";
 import "firebase/firestore";
 
 export const Page = () => {
-    const [messages, setMessages] = useState([]);
-    const [newMessage, setNewMessage] = useState("");
-
-    useEffect(() => {
-        const fetchMessages = async () => {
-            const snapshot = await firebase
-                .firestore()
-                .collection("messages")
-                .get();
-            const messagesData = snapshot.docs.map((doc) => doc.data());
-            setMessages(messagesData);
-        };
-        fetchMessages();
-    }, []);
-
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        if (newMessage.trim() === "") return;
-
-        await firebase.firestore().collection("messages").add({
-            text: newMessage.trim(),
-            timestamp: firebase.firestore.FieldValue.serverTimestamp(),
-        });
-
-        setNewMessage("");
-    };
-
     return (
         <div>
             <h1>Chat</h1>
             <ul>
-                {messages.map((message, index) => (
-                    <li key={index}>{message.text}</li>
-                ))}
+                <li>HOLA MUNDO</li>
             </ul>
-            <form onSubmit={handleSubmit}>
-                <input
-                    type="text"
-                    value={newMessage}
-                    onChange={(e) => setNewMessage(e.target.value)}
-                    placeholder="Escribe un mensaje..."
-                />
-                <button type="submit">Enviar</button>
-            </form>
         </div>
     );
 };
+
+// export const Page = () => {
+//     const [messages, setMessages] = useState([]);
+//     const [newMessage, setNewMessage] = useState("");
+
+//     useEffect(() => {
+//         const fetchMessages = async () => {
+//             const snapshot = await firebase
+//                 .firestore()
+//                 .collection("messages")
+//                 .get();
+//             const messagesData = snapshot.docs.map((doc) => doc.data());
+//             setMessages(messagesData);
+//         };
+//         fetchMessages();
+//     }, []);
+
+//     const handleSubmit = async (e) => {
+//         e.preventDefault();
+//         if (newMessage.trim() === "") return;
+
+//         await firebase.firestore().collection("messages").add({
+//             text: newMessage.trim(),
+//             timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+//         });
+
+//         setNewMessage("");
+//     };
+
+//     return (
+//         <div>
+//             <h1>Chat</h1>
+//             <ul>
+//                 {messages.map((message, index) => (
+//                     <li key={index}>{message.text}</li>
+//                 ))}
+//             </ul>
+//             <form onSubmit={handleSubmit}>
+//                 <input
+//                     type="text"
+//                     value={newMessage}
+//                     onChange={(e) => setNewMessage(e.target.value)}
+//                     placeholder="Escribe un mensaje..."
+//                 />
+//                 <button type="submit">Enviar</button>
+//             </form>
+//         </div>
+//     );
+// };
