0d7bd6b83936ce56e51fd08a07708772e25e7106
GonzaloArray <gonzalo.arrayaran@gmail.com>
2023-07-06 01:33:58 -0300

    feat: logic input empty error

--- src/auth/page/login/Page.jsx
+++ src/auth/page/login/Page.jsx
@@ -1,53 +1,21 @@
-import { Button, Container, TextField } from '@mui/material'
-import React from 'react'
-import { InputPassword } from '../../components/InputPassword'
-import { Separator } from '../../components/Separator'
-
-import { CloseAction } from '../../components/CloseAction'
-import { LoginFirebase } from '../../components/LoginFirebase'
-import { AccountDescription } from '../../components/AccountDescription'
-import { LogoDescription } from '../../components/LogoDescription'
+import { Container } from '@mui/material'
+import { useForm } from 'react-hook-form'
+import { ModalLogin } from '../../components/ModalLogin'
 
 export const Page = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm()
+
+  const eventSubmit = (data) => {
+    console.log(data)
+  }
+
   return (
     <Container maxWidth='sm' sx={{ marginTop: 2 }}>
-      <CloseAction />
-      <LogoDescription title='Logueate en Lowdy' />
-      <form>
-        <TextField
-          id='email'
-          label='Correo electrónico'
-          variant='standard'
-          type='email'
-          margin='normal'
-          fullWidth
-          color='secondary'
-          required
-          autoComplete='current-password'
-        />
-        <InputPassword />
-        <Button
-          sx={{
-            marginTop: 5,
-            textTransform: 'none',
-            fontSize: '16px',
-            p: '13px'
-          }}
-          size='large'
-          color='secondary'
-          variant='contained'
-          fullWidth
-        >
-          Iniciar sesión
-        </Button>
-      </form>
-      <Separator />
-      <LoginFirebase />
-      <AccountDescription
-        path='/register'
-        title='¿No tienes cuenta aún?'
-        linkName='Registrarse'
-      />
+      <ModalLogin register={register} submit={handleSubmit} errors={errors} event={eventSubmit} />
     </Container>
   )
 }
