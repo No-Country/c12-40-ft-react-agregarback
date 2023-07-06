a16f7c1fa8244b83ec6048903d3a048e99ad9f77
Francisco Carvajal Villegas <57154324+FranciscoCarvajal404@users.noreply.github.com>
2023-07-06 11:41:42 -0600

    feat: header arriba

--- src/client/page/dashboard/layout/LayoutDashboard.jsx
+++ src/client/page/dashboard/layout/LayoutDashboard.jsx
@@ -5,14 +5,16 @@ import { styled } from 'styled-components'
 const Layout = styled.div`
 
   display: flex;
-  width: 100%;
+  flex-direction: column;
+  width: 100vw;
+  height: 100vh;
 `
 
 const Aside = styled.aside`
 
   background-color: green;
 
-  min-height: 100vh;
+  height: 100%;
   max-width: 25%;
 
   display: flex;
@@ -35,42 +37,54 @@ const Aside = styled.aside`
 
 const InnerLayout = styled.div`
   display: flex;
-  flex-direction: column;
 
   width: 100%;
+  height: 100%;
 `
 
 const Header = styled.header`
 
   background-color: red;
 
+  padding: 16px;
+
+  width: 100%;
+
 `
 
 const Main = styled.main`
   background-color: blue;
 
+  padding: 32px;
+
   height: 100%;
+  width: 100%;
 `
 
 export const LayoutDashboard = () => {
   return (
     <Layout>
-      <Aside>
-        Aside
-        <ul>
-          <Link to={'.'}><li>Dashboard</li></Link>
-          <Link to={'opcion1'}><li>Opcion 1</li></Link>
-          <Link to={'opcion2'}><li>Opcion 2</li></Link>
-          <Link to={'opcion3'}><li>Opcion 3</li></Link>
-        </ul>
-      </Aside>
+
+      <Header>
+        Header
+      </Header>
+
       <InnerLayout>
-        <Header>
-          Header
-        </Header>
+
+        <Aside>
+          Aside
+          <ul>
+            <Link to={'.'}><li>Dashboard</li></Link>
+            <Link to={'opcion1'}><li>Opcion 1</li></Link>
+            <Link to={'opcion2'}><li>Opcion 2</li></Link>
+            <Link to={'opcion3'}><li>Opcion 3</li></Link>
+          </ul>
+        </Aside>
+
         <Main>
           <Outlet />
         </Main>
+
       </InnerLayout>
     </Layout>
   )
