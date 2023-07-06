b529e5efc12c58f5790ebf8996f11bfeb7aa560d
GonzaloArray <gonzalo.arrayaran@gmail.com>
2023-07-05 23:57:53 -0300

    fix: eslint config standar extension

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,17 +1,11 @@
 module.exports = {
   env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
-    './node_modules/eslint-config-standard/.eslintrc.json'
-  ],
+  extends: ['./node_modules/standard/eslintrc.json'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': 'warn',
-    'eslint-disable react/prop-types': 'on'
+    'react/prop-types': 'off'
   }
 }
