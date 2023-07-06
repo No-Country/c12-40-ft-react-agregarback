4892d0680cf0a59c15f3dc80511e29621055a357
GonzaloArray <gonzalo.arrayaran@gmail.com>
2023-07-06 01:40:29 -0300

    fix: eslint config standard

--- src/auth/components/Input.jsx
+++
@@ -1,8 +0,0 @@
-export const Input = ({type, name, change }) => {
-  return (
-    <div>
-      <label htmlFor={name}></label>
-      <input type={type} name={name} id={name} onChange={change} />
-    </div>
-  )
-}
