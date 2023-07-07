58ce0693b2a6c0e8ffd31846a25e28b06d8c91c3
TorlettiJoaquin <axeltorletti@gmail.com>
2023-07-07 20:31:12 -0300

    feat: styling chat

--- src/client/page/chat/components/Inputs.jsx
+++ src/client/page/chat/components/Inputs.jsx
@@ -8,15 +8,40 @@ const InputsSect = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
+  gap: 10px;
 
   input{
-    
+    width: 100%;
+    height: 100%;
+    outline: none;
+    font-size: 18px;
+    color: #2f2d52;
+
+    &::placeholder{
+      color: lightgray;
+    }
   }
 `
 
 const Send = styled.div`
+  height: 100%;
+  padding: 1px;
+  display: flex;
+  text-align: center;
+
+
   button{
+    display: flex;
+    align-items: center;
     cursor: pointer;
+    padding: 10px 15px;
+    background-color: #DA7FBB;
+    border-radius: 10px;
+
+    &:hover{
+      background-color: #CE55A5;
+      
+    }
   }
 `
 
--- src/client/page/chat/components/Message.jsx
+++ src/client/page/chat/components/Message.jsx
@@ -3,12 +3,62 @@ import { styled } from 'styled-components'
 
 
 const MessageSect = styled.div`
-  
+  display: flex;
+  gap: 20px;
+  margin-bottom: 20px;
+
+
+  &.owner{
+    flex-direction: row-reverse;
+
+    p{
+      border: 1px solid #707C9740;
+      background-color: none;
+      border-radius: 7px 7px 0 7px;
+    }
+  }
+
+`
+
+const MessageInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  color: gray;
+  font-weight: 300;
+
+  img{
+      width: 40px;
+      height: 40px;
+      border-radius: 50%;
+      object-fit: cover;
+    }
+`
+
+const MessageContent = styled.div`
+  max-width: 80%;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+
+  p{
+    background-color: white;
+    padding: 10px 20px;
+    border-radius: 0 7px 7px 7px;
+  }
 `
 
 const Message = () => {
   return (
-    <MessageSect>Message</MessageSect>
+    <MessageSect className='owner'>
+      <MessageInfo>
+        <img src="https://cdn3.iconfinder.com/data/icons/avatars-9/145/Avatar_Cat-1024.png" alt="" />
+        <span>just now</span>
+      </MessageInfo>
+      <MessageContent>
+        <p>hello</p>
+        {/* <img src="https://cdn3.iconfinder.com/data/icons/avatars-9/145/Avatar_Penguin-512.png" alt="" /> */}
+      </MessageContent>
+    </MessageSect>
   )
 }
 
