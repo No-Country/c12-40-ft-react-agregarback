f12dbcace0c4f74bbd11189461dfbe5b642dedc9
Francisco Carvajal Villegas <57154324+FranciscoCarvajal404@users.noreply.github.com>
2023-07-10 10:41:29 -0600

    feat: banner structure

--- src/client/page/profile/Page.jsx
+++ src/client/page/profile/Page.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
+import Banner from './components/Banner'
 
 export const Page = () => {
   return (
-    <>Profile</>
+    <Banner />
   )
 }
--- src/client/page/profile/components/Banner.jsx
+++ src/client/page/profile/components/Banner.jsx
@@ -1,7 +1,49 @@
 import React from 'react'
+import styled from 'styled-components'
 
-export const Page = () => {
+const BannerStyled = styled.header`
+
+  background-color: red;
+
+  width: 100%;
+
+  display: flex;
+  justify-content: space-between;
+
+  .person{
+    display: flex;
+  }
+
+  .person-info{
+    display: flex;
+    flex-direction: column;
+  }
+
+  .languages{
+    display: flex;
+  }
+`
+
+const Banner = () => {
   return (
-    <>Banner</>
+    <BannerStyled>
+
+      <div className='person'>
+        <img src='#' alt='Profile' />
+
+        <div className='person-info'>
+          Luciana Guillermina
+          <div className='languages'>
+            idioma | idioma 1 idioma 2
+          </div>
+        </div>
+      </div>
+
+      <div>
+        <img src='#' alt='editar' />
+      </div>
+    </BannerStyled>
   )
 }
+
+export default Banner
