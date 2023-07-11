b74291c2610255edb4acf5f58a6ad79f9570b2db
TorlettiJoaquin <axeltorletti@gmail.com>
2023-07-11 17:39:26 -0300

    feat: i18next operative

--- src/i18n.js
+++ src/i18n.js
@@ -1,16 +1,11 @@
 import i18n from 'i18next'
+import Backend from 'i18next-http-backend'
+import LanguageDetector from 'i18next-browser-languagedetector'
 import { initReactI18next } from 'react-i18next'
 
-import Backend from 'i18next-http-backend' // public/locales/{lang}/translation.json
-import LanguageDetector from 'i18next-browser-languagedetector'
-// don't want to use this?
-// have a look at the Quick start guide
-// for passing in lng and translations on init
-
 i18n
-  // load translation using http -> see /public/locales (i.e. https://github.com/i18next/react-i18next/tree/master/example/react/public/locales)
+  // load translation using http -> see /public/locales
   // learn more: https://github.com/i18next/i18next-http-backend
-  // want your translations to be loaded from a professional CDN? => https://github.com/locize/react-tutorial#step-2---use-the-locize-cdn
   .use(Backend)
   // detect user language
   // learn more: https://github.com/i18next/i18next-browser-languageDetector
@@ -20,11 +15,14 @@ i18n
   // init i18next
   // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
-    fallbackLng: 'es',
     debug: true,
-
+    fallbackLng: 'es',
     interpolation: {
       escapeValue: false // not needed for react as it escapes by default
+    },
+
+    react: {
+      useSuspense: false
     }
   })
 
--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import { store } from './common/store/store.js'
 import './App.css'
-import './translate/i18n'
+import './i18n'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
