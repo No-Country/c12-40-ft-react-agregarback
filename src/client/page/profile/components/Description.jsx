d283a86cf11ccb8208c96651aac6802cb4b8c792
Francisco Carvajal Villegas <57154324+FranciscoCarvajal404@users.noreply.github.com>
2023-07-12 10:52:51 -0600

    feat: Show more btn working

--- src/client/page/profile/components/Description.jsx
+++ src/client/page/profile/components/Description.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { styled } from 'styled-components'
 
 const DesciptionStyled = styled.section`
@@ -16,11 +16,18 @@ const DesciptionStyled = styled.section`
 `
 
 export const Page = () => {
+  const [showMore, setShowMore] = useState(false)
+
+  const text = 'Descricpion muy larga para ver si esto funciona Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+
   return (
     <DesciptionStyled>
       <div>
         <h2>Descripción</h2>
-        <p>Aquí va la descripción</p>
+        <h6>
+          {showMore ? text : `${text.substring(0, 130)}`}
+          <button className='btn' onClick={() => setShowMore(!showMore)}>{showMore ? 'Ver menos' : 'Ver más'}</button>
+        </h6>
       </div>
     </DesciptionStyled>
   )
