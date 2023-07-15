3b0004de668b67427842b21fc22bb815b593b364
GonzaloArray <gonzalo.arrayaran@gmail.com>
2023-07-15 00:26:17 -0300

    fix: resolve static date and not valid birthday +18

--- src/auth/page/step/models/Step4.jsx
+++ src/auth/page/step/models/Step4.jsx
@@ -1,25 +1,49 @@
 import { LocalizationProvider } from '@mui/x-date-pickers'
-import { DemoContainer } from '@mui/x-date-pickers/internals/demo'
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
 import { DatePicker } from '@mui/x-date-pickers/DatePicker'
-import React, { useState } from 'react'
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
+import dayjs from 'dayjs'
 import { Title } from '../../../components/Title'
+import { useState, useEffect } from 'react'
+import { TextField } from '@mui/material'
 
-export const Step4 = ({ register, errros }) => {
-  const [value, setValue] = useState(null)
+export const Step4 = ({ register, errors, watch, setValue }) => {
+  const [dateUser, setDateUser] = useState(null)
+
+  const validateDate = (date) => {
+    const maxDate = dayjs('2007-01-01')
+    return date.isAfter(maxDate)
+  }
+
+  useEffect(() => {
+    if (watch('selectedDate')) {
+      const result = dayjs(watch('selectedDate'))
+      setValue('selectedDate', result.valueOf(), { shouldValidate: true })
+      setDateUser(result)
+    }
+  }, [])
 
   return (
     <>
       <Title title='¿Cuándo naciste?' description='' />
       <LocalizationProvider dateAdapter={AdapterDayjs}>
-        <DemoContainer components={['DatePicker']}>
-          <DatePicker
-            label='Día de cumpleaños'
-            value={value}
-            onChange={(newValue) => setValue(newValue)}
-            slotProps={{ textField: { fullWidth: true } }}
-          />
-        </DemoContainer>
+        <DatePicker
+          label='Día de cumpleaños'
+          value={dateUser ?? null}
+          onChange={(date) => {
+            setDateUser(date)
+            setValue('selectedDate', date.valueOf(), { shouldValidate: true })
+          }}
+          shouldDisableDate={validateDate}
+          renderInput={(params) => (
+            <TextField
+              {...params}
+              {...register('selectedDate', { required: true })}
+              error={!!errors.selectedDate}
+              helperText={errors.selectedDate && errors.selectedDate.message}
+              fullWidth
+            />
+          )}
+        />
       </LocalizationProvider>
     </>
   )
