fc6b0c8b95ed004f6cd5c74dd33a5f371b85b1a7
GonzaloArray <gonzalo.arrayaran@gmail.com>
2023-07-16 04:14:40 -0300

    fix: fixing thunk implement

--- src/auth/store/thunk/thunk.js
+++ src/auth/store/thunk/thunk.js
@@ -2,6 +2,7 @@ import {
   onAuthenticatedAutomatic,
   onGoogleAuth,
   onLogout,
+  signIn,
   signUp
 } from '../../../service/firebaseAuth'
 
@@ -11,6 +12,20 @@ import {
   loginError,
   logout
 } from '../slice/sliceAuth'
+
+export const loginAuthentication = (data) => async (dispatch) => {
+  const { email, password } = data
+
+  dispatch(checkingCredentials())
+
+  const result = await signIn(email, password)
+
+  if (result.success) {
+    dispatch(login(result.userFirebase))
+  } else {
+    dispatch(loginError(result.error))
+  }
+}
 
 export const registerAuthentication = (data) => async (dispatch) => {
   const { email, password, name } = data
@@ -30,10 +45,8 @@ export const registerAuthenticationGoogle = () => async (dispatch) => {
   dispatch(checkingCredentials())
 
   const result = await onGoogleAuth()
-  console.log(result)
-  if (result.success) {
-    console.log(result)
 
+  if (result.success) {
     dispatch(login(result.userFirebase))
   } else {
     dispatch(loginError(result.error))
@@ -44,7 +57,7 @@ export const autheticatedAutomatic = () => async (dispatch) => {
   dispatch(checkingCredentials())
 
   const result = await onAuthenticatedAutomatic()
-  console.log(result)
+
   if (result.success) {
     dispatch(login(result.userFirebase))
   } else {
@@ -54,8 +67,6 @@ export const autheticatedAutomatic = () => async (dispatch) => {
 
 export const authenticatedLogout = () => (dispatch) => {
   dispatch(checkingCredentials())
-
   onLogout()
-
   dispatch(logout())
 }
