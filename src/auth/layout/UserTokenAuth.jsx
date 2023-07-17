d807ef2157db130724c430afd07f4ce080e8c270
GonzaloArray <gonzalo.arrayaran@gmail.com>
2023-07-17 00:12:05 -0300

    remove: console.log

--- src/auth/layout/UserTokenAuth.jsx
+++ src/auth/layout/UserTokenAuth.jsx
@@ -4,7 +4,6 @@ import { Navigate, Outlet } from 'react-router-dom'
 
 export const UserTokenAuth = () => {
   const auth = useAppSelector((state) => state.auth.user.token)
-  console.log(auth)
 
   return (!auth) ? <Outlet /> : <Navigate to='/' />
 }
