d8e44c1d1d49b661b88ce17e29c10b09e6ef6dcf
GonzaloArray <gonzalo.arrayaran@gmail.com>
2023-07-18 15:31:30 -0300

    fix: avatar profile

--- src/client/page/dashboard/Page.jsx
+++ src/client/page/dashboard/Page.jsx
@@ -3,8 +3,13 @@ import { SearchContained } from './components/SearchContained'
 import { ContainedPost } from '../../../common/style/container/ContainedPost'
 import { PublicComment } from './models/PublicComment'
 import { TitleSeparator } from './components/TitleSeparator'
+import { useAppSelector } from '../../../common/store/config'
 
 export const Page = () => {
+  const uid = useAppSelector(state => state.auth.user.user.uid)
+
+  console.log(uid)
+
   return (
     <ContainedPost>
       <SearchContained />
