0cd983dbe64ecb43cd4fbb7c3b10240b8b1812a3
Francisco Carvajal Villegas <57154324+FranciscoCarvajal404@users.noreply.github.com>
2023-07-24 13:32:34 -0600

    feat: achivement component estructura terminada

--- src/client/page/profile/components/Achivements.jsx
+++ src/client/page/profile/components/Achivements.jsx
@@ -22,14 +22,30 @@ const AchivementsStyled = styled.section`
 
     margin-bottom: 100px;
 
+    
     .outer-div{
-        border: 0.25px solid #A2CD37;
-        border-radius: 5px;
-        padding: 0.5rem;
+      border: 0.25px solid #A2CD37;
+      border-radius: 5px;
+      padding: 1.5rem;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+    }
+
+    .achivements-list{
+      display: flex;
+      flex-wrap: wrap;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+
+      gap: 1rem;
     }
 
     h2{
         font-weight: bold;
+        margin-bottom: 1.5rem;
     }
 
 `
@@ -38,6 +54,34 @@ const data = [
   {
     img: usage,
     label: '15 días de uso'
+  },
+  {
+    img: corrections,
+    label: '0 correcciones'
+  },
+  {
+    img: ai,
+    label: 'Usó Howdy AI'
+  },
+  {
+    img: posts,
+    label: '0 Publicaciones'
+  },
+  {
+    img: rate,
+    label: 'Calificó amigos'
+  },
+  {
+    img: recommend,
+    label: 'Recomendó Howdy'
+  },
+  {
+    img: trad,
+    label: '0 traducciones'
+  },
+  {
+    img: chat,
+    label: '0 conversaciones'
   }
 ]
 
@@ -46,7 +90,7 @@ export const Achivements = () => {
     <AchivementsStyled data={data}>
       <div className='outer-div'>
         <h2>Logros</h2>
-        <div>
+        <div className='achivements-list'>
           {
             data.map((data, i) => {
               return <AchivementBadge key={i} label={data?.label} img={data?.img} />
