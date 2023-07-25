a53fbc43cf5cfb7dca2489cd5032b8f5545d0ec0
Francisco Carvajal Villegas <57154324+FranciscoCarvajal404@users.noreply.github.com>
2023-07-25 11:22:43 -0600

    feat: earned badge basic style

--- src/client/page/profile/components/AchivementBadge.jsx
+++ src/client/page/profile/components/AchivementBadge.jsx
@@ -3,31 +3,44 @@ import { styled } from 'styled-components'
 
 const AchivementBadgeStyled = styled.div`
 
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  border: 1px solid #79747E ;
-  background-color: #F5F5F5 ;
+  .badge-div{
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      border: 1px solid #79747E ;
+      background-color: #F5F5F5 ;
+      width: 100%;
+      height: 100%;
+      
+      padding: 0.5rem 1rem;
+      gap: 0.5rem ;
+      
+      border-radius: 6.25rem ;
 
-  padding: 0.5rem 1rem;
-  gap: 0.5rem ;
-
-  border-radius: 6.25rem ;
-
-  img{
-    width: 1.125rem;
+      img{
+        width: 1.125rem;
+      }
+    
+      span{
+        font-size: 1rem ;
+      }
   }
 
-  span{
-    font-size: 1rem ;
+  .earned{
+    background-color: #F5F8EC;
+    color: #517C1A;
+    font-weight: bold;
   }
+
 `
 
 export default function AchivementBadge ({ img, label }) {
   return (
     <AchivementBadgeStyled>
-      <img src={img} alt='icon' />
-      <span>{label}</span>
+      <div className='badge-div earned'>
+        <img src={img} alt='icon' />
+        <span>{label}</span>
+      </div>
     </AchivementBadgeStyled>
   )
 }
