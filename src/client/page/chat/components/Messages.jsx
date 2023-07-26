ca01176542ca9ed95405a1fba4c85e865f00f726
GonzaloArray <gonzalo.arrayaran@gmail.com>
2023-07-26 04:33:34 -0300

    fix: text long

--- src/client/page/chat/components/Messages.jsx
+++ src/client/page/chat/components/Messages.jsx
@@ -2,10 +2,30 @@ import Message from './Message'
 import { styled } from 'styled-components'
 
 const MessagesContainer = styled.div`
-  background-color: white;
-  padding: 10px;
+  display: flex;
+  flex-direction: column;
+  gap: 1.4rem;
+  background-color: #f6f7d3fc;
+  padding: 20px;
   height: 70vh;
+  max-width: 100%;
   overflow-y: auto;
+  overflow-x: hidden;
+
+  &::-webkit-scrollbar {
+    width: 10px;
+  }
+
+  &::-webkit-scrollbar-track {
+    background-color: #c2e9fb;
+  }
+
+  &::-webkit-scrollbar-thumb {
+    background-color: #c70e7a;
+    border-radius: 4px;
+  }
+
+  scrollbar-color: #59a3cc #c2e9fb;
 `
 
 const Messages = ({ messages }) => {
