7d5d4adcae020af100817e8fb53a243539c96fa7
GonzaloArray <gonzalo.arrayaran@gmail.com>
2023-07-27 13:58:58 -0300

    fix: remove useEffect

--- src/client/page/dashboard/components/post/CommentRecentUser.jsx
+++ src/client/page/dashboard/components/post/CommentRecentUser.jsx
@@ -1,37 +1,23 @@
-import { collection, onSnapshot, orderBy, query, where } from 'firebase/firestore'
-import React, { useEffect } from 'react'
-import { db } from '../../../../../service/firebase'
-import { Grid, Typography } from '@mui/material'
+import { Grid, IconButton, Typography } from '@mui/material'
+import styled from 'styled-components'
+import MoreVertIcon from '@mui/icons-material/MoreVert'
 
-export const CommentRecentUser = ({ idPost, setComments, comments }) => {
-  useEffect(() => {
-    const q = query(
-      collection(db, 'comments'),
-      where('idPost', '==', idPost),
-      orderBy('createdAt', 'desc')
-    )
+const Text = styled(Typography)`
+  word-wrap: break-word;
+`
 
-    const unsubscribe = onSnapshot(q, (querySnapshot) => {
-      const data = []
-      querySnapshot.forEach((doc) => {
-        data.push({ id: doc.id, ...doc.data() })
-      })
-
-      setComments(data)
-    })
-
-    return () => unsubscribe()
-  }, [])
-
+export const CommentRecentUser = ({ comments }) => {
   return (
     <>
       {comments?.map(c => (
         <Grid container key={c.id} sx={{ p: 2 }}>
           <Grid item xs={11}>
-            <Typography variant='body1'>{c.msg}</Typography>
+            <Text variant='body1'>{c.msg}</Text>
           </Grid>
           <Grid item xs={1}>
-            <Typography>Algo</Typography>
+            <IconButton>
+              <MoreVertIcon />
+            </IconButton>
           </Grid>
         </Grid>
       ))}
