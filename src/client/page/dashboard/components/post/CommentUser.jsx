cee618837c547684bd94cbf9215e12af90cb06fe
Francisco Carvajal Villegas <57154324+FranciscoCarvajal404@users.noreply.github.com>
2023-07-26 20:59:12 -0600

    feat: CommentUser styles added

--- src/client/page/dashboard/components/post/CommentUser.jsx
+++ src/client/page/dashboard/components/post/CommentUser.jsx
@@ -1,14 +1,32 @@
-import { TextField } from '@mui/material'
+import { InputBase, Paper, TextField } from '@mui/material'
 import React from 'react'
 import { useForm } from 'react-hook-form'
 import { db } from '../../../../../service/firebase'
 import { addDoc, collection, serverTimestamp } from 'firebase/firestore'
 import { useAppSelector } from '../../../../../common/store/config'
 
+import addPic from '../../img/add-pic.svg'
+import SendIcon from '@mui/icons-material/Send'
+
 import { styled } from 'styled-components'
-const FormStyled = styled.form`
-  .comment-input{
-    border-radius: 100% !important;
+const FormStyled = styled(Paper)`
+
+  .comments-input{
+    padding: 1rem;
+  }
+
+  .icons-div{
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+    gap: 0.5rem;
+    
+  }
+
+  .icon-comment{
+    width: 1.5rem;
+    height: 1.5rem;
+    margin: 0;
   }
 `
 
@@ -37,9 +55,9 @@ export const CommentUser = ({ idPost, setComment }) => {
   }
 
   return (
-    <FormStyled onSubmit={handleSubmit(onHandle)} style={{ width: '100%' }}>
+    <FormStyled component='form' onSubmit={handleSubmit(onHandle)} style={{ display: 'flex', alignItems: 'center', width: '100%', borderRadius: '28px', padding: '0.25rem 1rem' }} elevation={0} variant='outlined'>
 
-      <TextField
+      <InputBase
         fullWidth
         name='comment'
         {...register('comment', { required: true })}
@@ -48,8 +66,14 @@ export const CommentUser = ({ idPost, setComment }) => {
         multiline
         placeholder='Añadir un comentario'
         className='comment-input'
+        style={{ padding: '0.25rem 0', width: '100%' }}
       />
-      <button type='submit' disabled={isValid} variant='contained'>Enviar</button>
+      <div className='icons-div'>
+        <img src={addPic} alt='Add picture' className='icon-comment' />
+        <button type='submit' disabled={isValid} variant='contained' className='icon-comment'>
+          <SendIcon />
+        </button>
+      </div>
 
     </FormStyled>
   )
