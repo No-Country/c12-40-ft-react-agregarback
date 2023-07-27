6ce306d50cc95fe7450f68ce11b340f7571aa4b6
Francisco Carvajal Villegas <57154324+FranciscoCarvajal404@users.noreply.github.com>
2023-07-26 19:26:28 -0600

    feat: showMore DesrciptionPost added

--- src/client/page/dashboard/components/post/DescriptionPost.jsx
+++ src/client/page/dashboard/components/post/DescriptionPost.jsx
@@ -1,10 +1,28 @@
-import { Button, Typography } from '@mui/material'
-import React from 'react'
+import React, { useState } from 'react'
+
+import { styled } from 'styled-components'
+
+const Paragraph = styled.p`
+    font-size: 0.875rem;
+    line-height: 1.25rem;
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+
+    .btn{
+      display: inline-block;
+      align-self: flex-end;
+      cursor: pointer;
+    }
+`
 
 export const DescriptionPost = ({ description }) => {
+  const [showMore, setShowMore] = useState(false)
+
   return (
-    <Typography variant='body1'>
-      {description} <Button color='secondary'> ver más</Button>
-    </Typography>
+    <Paragraph>
+      {showMore ? description : `${description.substring(0, 100)}...`}
+      <span className='btn' onClick={() => setShowMore(!showMore)}>{showMore ? ' ...ver menos' : ' ...ver más'}</span>
+    </Paragraph>
   )
 }
