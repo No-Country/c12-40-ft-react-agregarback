1ebe543ca253f9ff59bd4ea22eda357968144999
TomasCastagno <tomascastagno@live.com.ar>
2023-07-28 13:39:02 -0300

    section blog add

--- src/client/page/blog/Page.jsx
+++ src/client/page/blog/Page.jsx
@@ -1,10 +1,79 @@
 import React from "react";
-import { LayoutDashboard } from "../dashboard/layout/LayoutDashboard";
+import { Search } from "@mui/icons-material";
+import {
+  neutral,
+  neutral10,
+  primary,
+  primary120,
+  primary20,
+  primary25,
+  secondary120,
+} from "../../../common/variables";
+import { styled } from "styled-components";
+import { Container, Link } from "@mui/material";
+
+const ContainerSearch = styled.div`
+  position: relative;
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const InputSearch = styled.input`
+  color: ${primary};
+  background: #fcf1f8;
+  border-radius: 20px;
+  width: 450px;
+  height: 50px;
+  padding: 20px;
+  margin: 10px;
+`;
+
+const SearchIcon = styled(Search)`
+  position: absolute;
+  margin: auto;
+  right: 20px;
+  color: ${primary};
+  pointer-events: none;
+`;
+
+const Box = styled.div`
+  width: 300px;
+  height: 300px;
+  border-radius: 20px;
+  background: #da7fbb;
+`;
+
+const Title = styled.h1`
+  font-size: 25px;
+  color: primary;
+`;
 
 export const Page = () => {
   return (
-    <div>
-      <h1>Esta es la sección blog</h1>
-    </div>
+    <>
+      <Container style={{ display: "flex", justifyContent: "center" }}>
+        <ContainerSearch>
+          <InputSearch type="search" placeholder="Buscar" />
+          <SearchIcon />
+        </ContainerSearch>
+      </Container>
+
+      <Container
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          margin: "auto",
+          width: "100vw",
+          gap: "20px",
+        }}
+      >
+        <Box />
+        <Title>5 maneras creativas para prácticar y mejorar un idioma</Title>
+        <Link style={{ color: "#da7fbb" }}>Lee la guía completa</Link>
+      </Container>
+    </>
   );
 };
